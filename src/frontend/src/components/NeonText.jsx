789ddb0c20de7f228ba19b0b9241b4672cc039bb
Martin Munilla <martinmunillas@gmail.com>
2020-11-14 15:38:27 -0300

    Now NeonText blinking stops again but when the mouse is over it it starts again

--- src/frontend/src/components/NeonText.jsx
+++ src/frontend/src/components/NeonText.jsx
@@ -2,16 +2,16 @@ import React from 'react';
 import { useState } from 'react';
 
 const NeonLetter = ({ children }) => {
-  const [, setState] = useState(1);
-
-  const handleHover = () => {
-    setState(Math.random);
+  const handleHover = (e) => {
+    e.target.style.animation = 'none'
+    e.target.offsetHeight;
+    e.target.style.animation = `text-flicker-in-glow ${Math.random() * 1}s linear both`
   };
 
   return (
     <span
       style={{
-        animation: `text-flicker-in-glow ${Math.random() * 10}s linear both infinite `,
+        animation: `text-flicker-in-glow ${Math.random() * 10}s linear both`,
         color: `hsla(${Math.random() * 360}, 100%, 80%, 1)`,
       }}
       onMouseOver={handleHover}
